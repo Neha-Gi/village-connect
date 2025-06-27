68bb1f07cae5ddf1b1aa660fe1249ea3bd3e0f70
Neha Girme <153274867+Neha-Gi@users.noreply.github.com>
2025-06-27 12:00:48 +0200

    Update settings.py

--- village_connect/settings.py
+++ village_connect/settings.py
@@ -87,16 +87,18 @@ WSGI_APPLICATION = 'village_connect.wsgi.application'
 ASGI_APPLICATION = 'village_connect.asgi.application'
 
 # Database
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'village_connect_db',
-        'USER': 'village_user',
-        'PASSWORD': 'password123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
+
 
 
 # Custom User Model
